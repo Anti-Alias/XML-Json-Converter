3eacc774fb0b5d6dd84fa63328f2f8ac39a1a8da
William Cahill <drewlikesramen@gmail.com>
2021-05-31 20:21:14 -0400

    Refactoring conversion code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,49 +18,15 @@ class App {
     private static void start(String[] args) throws IOException {
 
         // Checks/massages arguments
-        if(args.length != 3)
+        if (args.length != 3)
             throw new IllegalArgumentException("Invalid number of CLI arguments. Expected 3 <source-file-name> <dest-file-name> <dest-format>");
         File sourceFile = new File(args[0]);
         File destFile = new File(args[1]);
-        String destinationFormat = args[2].toLowerCase();
-        validateArgs(sourceFile, destFile, destinationFormat);
+        String destFormat = args[2].toLowerCase();
+        validateArgs(sourceFile, destFile, destFormat);
 
-        // Determines source / dest formats
-        String sourceFormat;
-        if(destinationFormat.equals("xml"))
-            sourceFormat = "json";
-        else if(destinationFormat.equals("json"))
-            sourceFormat = "xml";
-        else
-            throw new IllegalArgumentException("Invalid destination format '" + destinationFormat + "'");
-
-        // Opens files and converts
-        try(Reader input = new InputStreamReader(new FileInputStream(sourceFile), StandardCharsets.UTF_8)) {
-            try(Writer output = new OutputStreamWriter(new FileOutputStream(destFile), StandardCharsets.UTF_8)) {
-                convert(input, output, sourceFormat, destinationFormat);
-            }
-        }
-    }
-
-    private static void convert(Reader input, Writer output, String sourceFormat, String destinationFormat) throws IOException {
-
-        // Creates object mappers
-        ObjectMapper inputMapper = objectMapperFor(sourceFormat);
-        ObjectMapper outputMapper = objectMapperFor(destinationFormat);
-
-        // Reads from input and converts it to a POJO
-        AddressBook book = inputMapper.readValue(input, AddressBook.class);
-        outputMapper.writerWithDefaultPrettyPrinter().writeValue(output, book);
-    }
-
-    private static ObjectMapper objectMapperFor(String format) {
-        if(format.equals("xml")) {
-            return new XmlMapper();
-        }
-        else if(format.equals("json")) {
-            return new ObjectMapper();
-        }
-        else throw new IllegalArgumentException("Invalid format '" + format + "'");
+        // Does conversion
+        Converter.convert(sourceFile, destFile, destFormat);
     }
 
     private static void validateArgs(File sourceFile, File destFile, String destinationFormat) {
---
+++ src/main/java/Converter.java
@@ -0,0 +1,48 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Converter {
+
+    public static void convert(File sourceFile, File destFile, String destFormat) throws IOException {
+
+        // Determines source / dest formats
+        String sourceFormat;
+        if(destFormat.equals("xml"))
+            sourceFormat = "json";
+        else if(destFormat.equals("json"))
+            sourceFormat = "xml";
+        else
+            throw new IllegalArgumentException("Invalid destination format '" + destFormat + "'");
+
+        // Opens files and converts
+        try(Reader input = new InputStreamReader(new FileInputStream(sourceFile), StandardCharsets.UTF_8)) {
+            try(Writer output = new OutputStreamWriter(new FileOutputStream(destFile), StandardCharsets.UTF_8)) {
+                convert(input, output, sourceFormat, destFormat);
+            }
+        }
+    }
+
+    public static void convert(Reader input, Writer output, String sourceFormat, String destinationFormat) throws IOException {
+
+        // Creates object mappers
+        ObjectMapper inputMapper = objectMapperFor(sourceFormat);
+        ObjectMapper outputMapper = objectMapperFor(destinationFormat);
+
+        // Reads from input and converts it to a POJO
+        AddressBook book = inputMapper.readValue(input, AddressBook.class);
+        outputMapper.writerWithDefaultPrettyPrinter().writeValue(output, book);
+    }
+
+    private static ObjectMapper objectMapperFor(String format) {
+        if(format.equals("xml")) {
+            return new XmlMapper();
+        }
+        else if(format.equals("json")) {
+            return new ObjectMapper();
+        }
+        else throw new IllegalArgumentException("Invalid format '" + format + "'");
+    }
+}
